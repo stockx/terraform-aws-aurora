ed6ad7a94e5757d49a2cfdf6c306e4ed569039d0
Ofiliojo Ichaba <ofili@stockx.com>
2020-03-18 17:58:48 -0700

    Update alarms.tf to Terraform 0.12

--- alarms.tf
+++ alarms.tf
@@ -1,5 +1,5 @@
 resource "aws_cloudwatch_metric_alarm" "alarm_rds_DatabaseConnections_writer" {
-  count               = "${var.cw_alarms ? 1 : 0}"
+  count               = var.cw_alarms ? 1 : 0
   alarm_name          = "${aws_rds_cluster.default.id}-alarm-rds-writer-DatabaseConnections"
   comparison_operator = "GreaterThanOrEqualToThreshold"
   evaluation_periods  = "1"
@@ -7,19 +7,19 @@ resource "aws_cloudwatch_metric_alarm" "alarm_rds_DatabaseConnections_writer" {
   namespace           = "AWS/RDS"
   period              = "60"
   statistic           = "Sum"
-  threshold           = "${var.cw_max_conns}"
+  threshold           = var.cw_max_conns
   alarm_description   = "RDS Maximum connection Alarm for ${aws_rds_cluster.default.id} writer"
-  alarm_actions       = ["${var.cw_sns_topic}"]
-  ok_actions          = ["${var.cw_sns_topic}"]
+  alarm_actions       = [var.cw_sns_topic]
+  ok_actions          = [var.cw_sns_topic]
 
-  dimensions {
-    DBClusterIdentifier = "${aws_rds_cluster.default.id}"
+  dimensions = {
+    DBClusterIdentifier = aws_rds_cluster.default.id
     Role                = "WRITER"
   }
 }
 
 resource "aws_cloudwatch_metric_alarm" "alarm_rds_DatabaseConnections_reader" {
-  count               = "${var.cw_alarms && var.replica_count > 0 ? 1 : 0}"
+  count               = var.cw_alarms && var.replica_count > 0 ? 1 : 0
   alarm_name          = "${aws_rds_cluster.default.id}-alarm-rds-reader-DatabaseConnections"
   comparison_operator = "GreaterThanOrEqualToThreshold"
   evaluation_periods  = "1"
@@ -27,19 +27,19 @@ resource "aws_cloudwatch_metric_alarm" "alarm_rds_DatabaseConnections_reader" {
   namespace           = "AWS/RDS"
   period              = "60"
   statistic           = "Maximum"
-  threshold           = "${var.cw_max_conns}"
+  threshold           = var.cw_max_conns
   alarm_description   = "RDS Maximum connection Alarm for ${aws_rds_cluster.default.id} reader(s)"
-  alarm_actions       = ["${var.cw_sns_topic}"]
-  ok_actions          = ["${var.cw_sns_topic}"]
+  alarm_actions       = [var.cw_sns_topic]
+  ok_actions          = [var.cw_sns_topic]
 
-  dimensions {
-    DBClusterIdentifier = "${aws_rds_cluster.default.id}"
+  dimensions = {
+    DBClusterIdentifier = aws_rds_cluster.default.id
     Role                = "READER"
   }
 }
 
 resource "aws_cloudwatch_metric_alarm" "alarm_rds_CPU_writer" {
-  count               = "${var.cw_alarms ? 1 : 0}"
+  count               = var.cw_alarms ? 1 : 0
   alarm_name          = "${aws_rds_cluster.default.id}-alarm-rds-writer-CPU"
   comparison_operator = "GreaterThanOrEqualToThreshold"
   evaluation_periods  = "2"
@@ -47,19 +47,19 @@ resource "aws_cloudwatch_metric_alarm" "alarm_rds_CPU_writer" {
   namespace           = "AWS/RDS"
   period              = "60"
   statistic           = "Maximum"
-  threshold           = "${var.cw_max_cpu}"
+  threshold           = var.cw_max_cpu
   alarm_description   = "RDS CPU Alarm for ${aws_rds_cluster.default.id} writer"
-  alarm_actions       = ["${var.cw_sns_topic}"]
-  ok_actions          = ["${var.cw_sns_topic}"]
+  alarm_actions       = [var.cw_sns_topic]
+  ok_actions          = [var.cw_sns_topic]
 
-  dimensions {
-    DBClusterIdentifier = "${aws_rds_cluster.default.id}"
+  dimensions = {
+    DBClusterIdentifier = aws_rds_cluster.default.id
     Role                = "WRITER"
   }
 }
 
 resource "aws_cloudwatch_metric_alarm" "alarm_rds_CPU_reader" {
-  count               = "${var.cw_alarms && var.replica_count > 0 ? 1 : 0}"
+  count               = var.cw_alarms && var.replica_count > 0 ? 1 : 0
   alarm_name          = "${aws_rds_cluster.default.id}-alarm-rds-reader-CPU"
   comparison_operator = "GreaterThanOrEqualToThreshold"
   evaluation_periods  = "2"
@@ -67,19 +67,19 @@ resource "aws_cloudwatch_metric_alarm" "alarm_rds_CPU_reader" {
   namespace           = "AWS/RDS"
   period              = "60"
   statistic           = "Maximum"
-  threshold           = "${var.cw_max_cpu}"
+  threshold           = var.cw_max_cpu
   alarm_description   = "RDS CPU Alarm for ${aws_rds_cluster.default.id} reader(s)"
-  alarm_actions       = ["${var.cw_sns_topic}"]
-  ok_actions          = ["${var.cw_sns_topic}"]
+  alarm_actions       = [var.cw_sns_topic]
+  ok_actions          = [var.cw_sns_topic]
 
-  dimensions {
-    DBClusterIdentifier = "${aws_rds_cluster.default.id}"
+  dimensions = {
+    DBClusterIdentifier = aws_rds_cluster.default.id
     Role                = "READER"
   }
 }
 
 resource "aws_cloudwatch_metric_alarm" "alarm_rds_replica_lag" {
-  count               = "${var.cw_alarms && var.replica_count > 0 ? 1 : 0}"
+  count               = var.cw_alarms && var.replica_count > 0 ? 1 : 0
   alarm_name          = "${aws_rds_cluster.default.id}-alarm-rds-reader-AuroraReplicaLag"
   comparison_operator = "GreaterThanOrEqualToThreshold"
   evaluation_periods  = "5"
@@ -87,13 +87,13 @@ resource "aws_cloudwatch_metric_alarm" "alarm_rds_replica_lag" {
   namespace           = "AWS/RDS"
   period              = "60"
   statistic           = "Maximum"
-  threshold           = "${var.cw_max_replica_lag}"
+  threshold           = var.cw_max_replica_lag
   alarm_description   = "RDS CPU Alarm for ${aws_rds_cluster.default.id}"
-  alarm_actions       = ["${var.cw_sns_topic}"]
-  ok_actions          = ["${var.cw_sns_topic}"]
+  alarm_actions       = [var.cw_sns_topic]
+  ok_actions          = [var.cw_sns_topic]
 
-  dimensions {
-    DBClusterIdentifier = "${aws_rds_cluster.default.id}"
+  dimensions = {
+    DBClusterIdentifier = aws_rds_cluster.default.id
     Role                = "READER"
   }
 }
